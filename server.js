6b4d7e837c1c6096335cb46f3fc5a0db5957180f
sathwik-30 <cherukurusathwik30@gmail.com>
2026-04-30 20:16:29 +0530

    backend upload1

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 const allowedOrigins = [
  "http://localhost:3000",
  "http://localhost:5173",
- "https://fsadprofront-k6wm7suq9-sathwiks-projects-cb9cd622.vercel.app",
+ "https://fsadprofront.vercel.app",
  "https://*.vercel.app"
 ];
 
